0d2f89113945017cc063d76c40b64866224c1a86
edocsil47 <66987192+edocsil47@users.noreply.github.com>
2021-03-25 16:02:14 -0700

    Update wayforumDisagrements.js

--- wayforumDisagrements.js
+++ wayforumDisagrements.js
@@ -1,11 +1,16 @@
 // ==UserScript==
-// @id wfdisagree
-// @name Show Wayforum Disagreements
-// @version 0.1
-// @author /u/edocsil47
-// @description Lists your enemies on the forum
-// @match https://community.wayfarer.nianticlabs.com/discussion/*
-// @require http://code.jquery.com/jquery-latest.js
+// @id           pogoExtendedRange
+// @name         IITC Plugin: Pokemon GO Extended Range
+// @category     Tweaks
+// @version      0.2.0
+// @namespace    https://github.com/edocsil47/IITCPogoExtendedRange
+// @downloadURL  https://github.com/edocsil47/IITCPogoExtendedRange/raw/master/extendedRange.user.js
+// @homepageURL  https://github.com/edocsil47/IITCPogoExtendedRange
+// @description  Adds second circle for the new 80m Pokestop and Gym interaction range
+// @author       edocsil47
+// @include      https://intel.ingress.com/*
+// @match        https://intel.ingress.com/*
+// @grant        none
 // ==/UserScript==
 
 $("a.ReactButton-Disagree").attr("data-reaction", "disagree");
