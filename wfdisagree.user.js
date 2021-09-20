30d6e215fab09816849850937fa85a307b30c5d5
edocsil47 <66987192+edocsil47@users.noreply.github.com>
2021-09-20 13:13:07 -0700

    Update wfdisagree.user.js

--- wfdisagree.user.js
+++ wfdisagree.user.js
@@ -5,6 +5,7 @@
 // @author /u/edocsil47
 // @description Hover to see disagreements on the Ingress and Wayfarer community forums
 // @match https://community.wayfarer.nianticlabs.com/discussion/*
+// @match https://community.ingress.com/en/discussion/*
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
