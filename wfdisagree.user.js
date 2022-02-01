4d2ee243fadb96d2956a1aa96270893a30f54102
edocsil47 <66987192+edocsil47@users.noreply.github.com>
2022-01-31 19:08:18 -0800

    Update wfdisagree.user.js

--- wfdisagree.user.js
+++ wfdisagree.user.js
@@ -1,12 +1,48 @@
 // ==UserScript==
 // @id wfdisagree
-// @name Show Forum Disagreements
-// @version 0.1
+// @name Show Wayforum Reactions
+// @version 2.0
 // @author /u/edocsil47
-// @description Hover to see disagreements on the Ingress and Wayfarer community forums
-// @match https://community.wayfarer.nianticlabs.com/discussion/*
-// @match https://community.ingress.com/en/discussion/*
+// @description Reenable reactions on the wayfarer and ingress community forums
+// @match https://community.wayfarer.nianticlabs.com/*
+// @match https://community.ingress.com/*
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
+// reattach disagree data to the button; redundant attach others preemptively
+$("a.ReactButton-Insightful").attr("data-reaction", "insightful");
 $("a.ReactButton-Disagree").attr("data-reaction", "disagree");
+$("a.ReactButton-Like").attr("data-reaction", "like");
+
+// duplicate https://community.wayfarer.nianticlabs.com/plugins/Reactions/js/reactions.js?v=61f19289
+// but don't return early with whatever config they have set
+
+$(document).on('mouseenter', '.ReactButton', function() {
+   var $button = $(this);
+
+   var itemID = $button.closest('.Item').attr('id');
+   var $menu = $('.MenuItems-Reactions', $button);
+
+   if ($menu.length == 0) {
+		// Construct the initial div.
+		$menu = $('<div class="MenuItems MenuItems-Reactions Up"><div class="TinyProgress" /></div>')
+			.css('visibility', 'hidden')
+			.attr('aria-hidden', 'true')
+			.attr('tabindex', '-1')
+			.appendTo($button);
+
+		$.ajax({
+			url: gdn.url('/reactions/users/'+itemID.split('_').join('/')+'/'+$button.data('reaction')),
+			data: {DeliveryType: 'VIEW'},
+			success: function(data) {
+				$menu.html(data);
+			}
+		});
+   }
+
+   // Position the menu above the reaction button.
+   var left = ($button.outerWidth() - $menu.outerWidth()) / 2.0;
+   var bottom = $button.height();
+
+   $menu.css({ bottom: bottom, left: left, visibility: 'visible' });
+});
